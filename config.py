850901be82551a4543dc210086b4e98017c2ec7a
Kosoukhovsa <Kosoukhovsa@gmail.com>
2022-04-19 13:21:24 +0300

    add ipython report

--- config.py
+++ config.py
@@ -29,4 +29,7 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     #SQLALCHEMY_DATABASE_URI = 'postgres://nkvnpwkgmwvbxq:74691526b4438dfe472d020a544a88b5b1777d9735d50a963118ef7238ea09e9@ec2-79-125-26-232.eu-west-1.compute.amazonaws.com:5432/dc13mevsrvuj4j'
     #SQLALCHEMY_DATABASE_URI = 'postgres://'
+    # БД на HEROKU
     SQLALCHEMY_DATABASE_URI = 'postgres://sqdieqjwaeoagh:71f685cb5ad5c30d7c335e087ab918791a7c5b946b1a1be7c183c0679307b381@ec2-54-216-48-43.eu-west-1.compute.amazonaws.com:5432/d1e5qki0kv7tub'
+    # БД из бекап 
+    #SQLALCHEMY_DATABASE_URI = 'postgres://postgres:12345qwz@localhost:5433/ikp_restore'
